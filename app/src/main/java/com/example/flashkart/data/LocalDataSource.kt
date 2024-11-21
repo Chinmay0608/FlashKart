bc7dd46984b0cd89765dfa09062165c8d623b464
Chinmay Maheshwari <chinmay8064@gmail.com>
2024-11-21 11:37:38 +0530

    1

---
+++ app/src/main/java/com/example/flashkart/data/LocalDataSource.kt
@@ -0,0 +1,17 @@
+//package com.example.flashkart.data
+//
+//import com.example.flashkart.R
+//
+//object LocalDataSource {
+//    fun getItems():List<Item>{
+//        return listOf(
+//            Item(R.string.banana_robusta,R.string.fresh_fruits,"1 kg",100,R.drawable.banana_robusta),
+//            Item(R.string.shimla_apple,R.string.fresh_fruits,"1 kg",100,R.drawable.shimla_apple),
+//            Item(R.string.pineapple,R.string.fresh_fruits,"1 kg",100,R.drawable.pineapple),
+//            Item(R.string.pomegranate,R.string.fresh_fruits,"1 kg",100,R.drawable.pomegranate),
+//            Item(R.string.pepsi_can,R.string.beverages,"1",100,R.drawable.pepsi_can),
+//            Item(R.string.papaya_semi_ripe,R.string.fresh_fruits,"1 kg",100,R.drawable.papaya_semi_ripe),
+//            Item(R.string.nivea_shower_gel,R.string.bath_body,"1",100,R.drawable.nivea_shower_gel)
+//        )
+//    }
+//}
